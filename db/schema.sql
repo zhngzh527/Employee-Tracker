847be166b30382c525c51d5d36f0b8e0e446e692
ZHONGCHENG ZHAO <zhongchengzhao@ZHONGCHENGs-MBP.lan>
2022-12-08 22:27:03 -0600

    test

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS company;
+
+CREATE DATABASE company;
+USE company;
+
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    manager_id INT,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+USE company;
+
+INSERT INTO department(id, name)
+VALUES
+(1, "Sales"),
+(2, "Engineering"),
+(3, "Finance"),
+(4, "Legal");
+
+INSERT INTO roles(id, title, salary, department_id)
+VALUES
+(1, "Sales Lead", 100000, 1),
+(2, "Salesperson", 80000, 1),
+(3, "Lead Engineer", 150000, 2),
+(4, "Software Engineer", 120000, 2),
+(5, "Accountant", 125000, 3),
+(6, "Legal Team Lead", 250000, 4),
+(7, "Lawyer", 190000, 4);
+
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES
+(1, "John", "Doe", 1, 1),
+(2, "Mike", "Chan", 2, 2),
+(3, "Ashley", "Rodriguez", 3, 3),
+(4, "Kevin", "Tupik", 4, 1),
+(5, "Malia", "Brown", 5, 2),
+(6, "Sarah", "Lourd", 6, 3),
+(7, "Tom", "Allen", 7, 2),
+(8, "Christian", "Eckenrode", 3, 3);
